58f2c672f9bfb4c1e5bb6b31e3953b220b62f790
Paul O <paul.orawiec@gmail.com>
2019-11-12 09:34:00 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+
+  yield("A #{element1} and #{element2} sandwich")
+ 
+# Lewis Carroll's Kitchen
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
+
+end
+
+
+
