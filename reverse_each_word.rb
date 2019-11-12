649983368674dd074c749028df932f2c4dd73fdc
Paul O <paul.orawiec@gmail.com>
2019-11-12 10:01:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,10 @@
 def reverse_each_word(sentence)
   reversed_words = []
   words_array = sentence.split
+  
   words_array.collect do |x| reversed_words.push(x.reverse)
     end
 
   reversed_words.join(" ")
 
-#  yield("A #{element1} and #{element2} sandwich")
- 
-# Lewis Carroll's Kitchen
-#make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
-
 end
-
-
-
